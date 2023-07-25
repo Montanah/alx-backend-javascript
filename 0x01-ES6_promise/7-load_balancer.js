1ee7a9cd43f526840065d8a3bfa7d78e112fa9b5
Montanah <mititi.isaac@gmail.com>
2023-07-25 16:27:39 -0700

    Using promises for a Load balancer algorithm

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,14 +1,32 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-    return new Promise((resolve) => {
-      chinaDownload.then((china) => {
-        USDownload.then((US) => {
-          if (china > US) {
-            resolve(china);
+    const chinaServer = "China Server";
+    const usServer = "US Server";
+  
+    return new Promise((resolve, reject) => {
+      // Simulate asynchronous requests to the China and US servers
+      const chinaRequest = fetch(chinaDownload);
+      const usRequest = fetch(USDownload);
+  
+      // Use Promise.race to determine which server responds first
+      Promise.race([chinaRequest, usRequest])
+        .then((response) => {
+          // If the China server responded first
+          if (response === chinaRequest) {
+            resolve({
+              server: chinaServer,
+              response: response,
+            });
           } else {
-            resolve(US);
+            // If the US server responded first
+            resolve({
+              server: usServer,
+              response: response,
+            });
           }
+        })
+        .catch((error) => {
+          reject(error);
         });
-      });
     });
   }
   
