1216d67f3c5253f84e15df4e47d3d8fc8bfdbf8e
Montanah <mititi.isaac@gmail.com>
2023-07-25 16:52:55 -0700

    Throw error/try catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,8 @@
 export default function divideFunction(numerator, denominator) {
-    return new Promise((resolve, reject) => {
-      if (denominator === 0) {
-        throw new Error('cannot divide by 0');
-        } else {
-        resolve(numerator / denominator);
-      }
-    });
-}
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
+    } else {
+      return numerator / denominator;
+    }
+  }
+  
