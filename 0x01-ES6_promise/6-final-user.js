701235cd052a18ffad73b4eb114aac0d6303e846
Montanah <mititi.isaac@gmail.com>
2023-07-25 15:57:53 -0700

    Handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,25 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default handleProfileSignup(firstName, lastName, fileName); {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      for (const result of results) {
-        if (result.status === 'rejected') {
-          result.value = `Error: ${result.reason.message}`;
-          delete result.reason;
-        }
-      }
-      return results;
-    });
+export default handleProfileSignup(firstName, lastName, fileName) {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+    return Promise.allSettled([userPromise, photoPromise])
+        .then((results) => {
+            const finalResult = [];
+            for (const result of results) {
+                if (result.status === 'fulfilled') {
+                    finalResult.push({
+                        status: result.status,
+                        value: result.value,
+                    });
+                } else {
+                    finalResult.push({
+                        status: result.status,
+                        value: `Error: ${result.reason.message}`,
+                    });
+                }
+            }
+            return finalResult;
+        });
 }
