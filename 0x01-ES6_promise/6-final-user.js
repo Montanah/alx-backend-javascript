7f3466ed23ab901e623ce9059421360c75ec4e02
Montanah <mititi.isaac@gmail.com>
2023-07-28 09:58:42 +0000

    eslint checks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,24 +2,24 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const userPromise = signUpUser(firstName, lastName);
-    const photoPromise = uploadPhoto(fileName);
-    return Promise.allSettled([userPromise, photoPromise])
-        .then((results) => {
-            const finalResult = [];
-            for (const result of results) {
-                if (result.status === 'fulfilled') {
-                    finalResult.push({
-                        status: result.status,
-                        value: result.value,
-                    });
-                } else {
-                    finalResult.push({
-                        status: result.status,
-                        value: `Error: ${result.reason.message}`,
-                    });
-                }
-            }
-            return finalResult;
-        });
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      const finalResult = [];
+      for (const result of results) {
+        if (result.status === 'fulfilled') {
+          finalResult.push({
+            status: result.status,
+            value: result.value,
+          });
+        } else {
+          finalResult.push({
+            status: result.status,
+            value: `Error: ${result.reason.message}`,
+          });
+        }
+      }
+      return finalResult;
+    });
 }
