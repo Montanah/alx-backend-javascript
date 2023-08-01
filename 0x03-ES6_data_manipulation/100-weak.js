df9f9e92c524a4cacce96434a891279f779c440f
Montanah <mititi.isaac@gmail.com>
2023-08-01 15:35:35 -0700

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,6 @@
-export const weakMap = () => new WeakMap();
+export const weakMap = new WeakMap();
 
-export default function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
