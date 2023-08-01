e830235805d7ffb4f93f98a7d746eacbca23e026
Montanah <mititi.isaac@gmail.com>
2023-08-01 15:01:18 -0700

    Using startString, push and slice

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  if (typeof set !== 'object' || typeof startString !== 'string') {
+    return '';
+  }
+
+  const newSet = [];
+  set.forEach((value) => {
+    if (value && value.startsWith(startString)) {
+      newSet.push(value.slice(startString.length));
+    }
+  });
+
+  return newSet.join('-');
+}
