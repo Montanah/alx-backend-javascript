66a7c2ddcf9be7c1eece143b4f846e46406053cf
Montanah <mititi.isaac@gmail.com>
2023-08-11 12:01:43 -0700

    Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+// task_1/main.ts
+
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    readonly fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    readonly location: string;
+    [key: string]: any;
+  }
