d7c3973c335af180310ebae48e020491cafd154d
Montanah <mititi.isaac@gmail.com>
2023-08-11 12:19:25 -0700

    Task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,3 +8,45 @@ interface Teacher {
     readonly location: string;
     [key: string]: any;
   }
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string) {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+console.log(printTeacher("John", "Doe"));
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+class StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return "Currently working";
+    }
+
+    displayName() {
+        return this.firstName;
+    }
+}
