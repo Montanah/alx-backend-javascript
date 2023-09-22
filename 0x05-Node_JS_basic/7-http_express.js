523ebc07946c0fb80415671d72a0e817276280a5
Montanah <mititi.isaac@gmail.com>
2023-09-22 22:32:34 +0000

    Create a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,31 @@
+/* recreate the small HTTP server using Express:
+It should be assigned to the variable app and this one must be exported
+HTTP server should listen on port 1245
+It should return plain text
+When the URL path is /, it should display Hello Holberton School! in the page body
+When the URL path is /students, it should display This is the list of our students followed by the same content as the file 3-read_file_async.js (with and without the database) - the name of the database must be passed as argument of the file
+CSV file can contain empty lines (at the end) - and they are not a valid student!
+*/
+
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => {
+      res.send(`This is the list of our students\n${error.message}`);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
