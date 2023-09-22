2fbeb9b00bb044d6e998e880c4ed56bd017e3ec1
Montanah <mititi.isaac@gmail.com>
2023-09-22 21:52:26 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
-/* It should display the message Welcome to Holberton School, what is your name? (followed by a new line)
+/* It should display the message Welcome to Holberton School, what is your
+name? (followed by a new line)
 The user should be able to input their name on a new line
 The program should display Your name is: INPUT
-When the user ends the program, it should display This important software is now closing (followed by a new line)
-Your code will be tested through a child process, make sure you have everything you need for that
+When the user ends the program, it should display This important software is now
+closing (followed by a new line)
+Your code will be tested through a child process, make sure you have everything
+you need for that
 */
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
