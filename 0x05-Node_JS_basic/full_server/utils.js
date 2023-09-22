446b1a825462e0ac06719f1cc8f4837d9ebf3d15
Montanah <mititi.isaac@gmail.com>
2023-09-22 23:15:48 +0000

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,32 @@
+/* create a function named readDatabase that accepts a file path as argument:
+It should read the database asynchronously
+It should return a promise
+When the file is not accessible, it should reject the promise with the error
+When the file can be read, it should return an object of arrays of the firstname of students per fields
+*/
+
+const fs = require('fs');
+
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (!err) {
+        const fields = {};
+        const students = data.split('\n');
+        students.shift();
+        students.forEach((student) => {
+          if (student) {
+            const row = student.split(',');
+            if (!fields[row[3]]) fields[row[3]] = [];
+            fields[row[3]].push(row[0]);
+          }
+        });
+        resolve(fields);
+      } else {
+        reject(Error('Cannot load the database'));
+      }
+    });
+  });
+}
+
+module.exports = readDatabase;
