e3e6bfda8204145f6cdb8d3d7cfe1a0e3574ca40
Montanah <mititi.isaac@gmail.com>
2023-09-22 21:18:36 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,20 @@
-// Using Process stdin
-const readline = require('readline');
+/* It should display the message Welcome to Holberton School, what is your name? (followed by a new line)
+The user should be able to input their name on a new line
+The program should display Your name is: INPUT
+When the user ends the program, it should display This important software is now closing (followed by a new line)
+Your code will be tested through a child process, make sure you have everything you need for that
+*/
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${ answer}`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  console.log('This is important software is now closing');
-  process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
